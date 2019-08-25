44c779da41aa25d10a2b96da27f5074cfe6e8e7c
Ariel <kailichen99@gmail.com>
2019-08-26 11:53:10 +1200

    Paddle Moves

---
+++ Breakout Unity/Assets/Scripts/PaddleMovement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleMovement : MonoBehaviour
+{
+    public float paddleSpeed = 1f;
+
+    private Vector3 playerPos = new Vector3(-2, 0, -2);
+
+    private void Update()
+    {
+        float xPos = transform.position.x + (Input.GetAxis("Horizontal") * paddleSpeed);
+        playerPos = new Vector3(Mathf.Clamp(xPos, -14f, 10f), 0f, -2);
+        transform.position = playerPos;
+
+    }
+}
